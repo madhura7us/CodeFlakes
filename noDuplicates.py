49209477ac9dcc8c8f2a66de8a2bc63dcfff4e2c
Madhura G <madhura7us@gmail.com>
2018-11-21 19:12:41 -0500

    Remove duplicate items from an array

---
+++ noDuplicates.py
@@ -0,0 +1,8 @@
+# Given a sorted array nums, remove the duplicates return the new length.
+
+def removeDuplicates(arr):
+    x = list(set(arr))
+    print(x)
+    return len(x)
+            
+       
